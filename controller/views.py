cac6f1fb2db46bc2dc2df3fd49749ca28afe2e3f
Davi B Tonon <tonon1996@gmail.com>
2023-08-09 21:56:20 -0300

    Criação de filtros

--- controller/views.py
+++ controller/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from controller.models import Depoimentos, Destinos
 from controller.serializers import DepoimentosSerializers, DestinosSerializers
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, filters
+from rest_framework.response import Response
+
 # Create your views here.
 
 class DepoimentosViewSet(viewsets.ModelViewSet):
     """Classe para cria as views dos depoimentos"""
-
     queryset = Depoimentos.objects.all()
     serializer_class = DepoimentosSerializers
-
+   
 
 class DepoimentosAleatoriosViewSet(generics.ListAPIView):
     """Função para seleciona 3 depoimentos aleatorios"""
@@ -20,9 +21,27 @@ class DepoimentosAleatoriosViewSet(generics.ListAPIView):
     
     serializer_class = DepoimentosSerializers
 
-
 class DestinosViewSet(viewsets.ModelViewSet):
     "Classe para exibir os destinos"
     
     queryset = Destinos.objects.all()
     serializer_class = DestinosSerializers
+    filter_backends  = [filters.SearchFilter]
+    search_fields = ['nome']
+
+    def list(self, request, *args, **kwargs):
+        # Função que modifica o list para apresentar uma messagem de erro caso não encontre nda
+
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        if not queryset.exists():
+            return Response({"message": "Nenhum resultado encontrado."})
+        
+        return Response(serializer.data)
